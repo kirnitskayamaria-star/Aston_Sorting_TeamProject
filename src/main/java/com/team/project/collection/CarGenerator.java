07cb0c775d2de785106458eec9af17052e30d2d3
mariakirnitskaya <maria105@mail.ru>
2026-04-03 00:37:09 +0700

    Выделила числа в отдельные константы

--- src/main/java/com/team/project/collection/CarGenerator.java
+++ src/main/java/com/team/project/collection/CarGenerator.java
@@ -5,11 +5,17 @@ import java.util.Random;
 public class CarGenerator {
     private static final String[] MODELS = {"Lada", "Toyota", "BMW", "Audi", "Peugeot", "Lexus", "Mercedes", "Ford"};
     private static final Random RANDOM = new Random();
+
+    private static final int MAX_ADDITIONAL_POWER = 300;
+    private static final int MIN_POWER = 50;
+    private static final int MAX_YEAR_RANGE = 30;
+    private static final int MIN_YEAR = 1996;
+
     public static Car generateRandomCar(){
         return new Car.CarBuilder()
                 .model(MODELS[RANDOM.nextInt(MODELS.length)])
-                .power(RANDOM.nextInt(300) + 50)
-                .year(RANDOM.nextInt(30) + 1996)
+                .power(RANDOM.nextInt(MAX_ADDITIONAL_POWER) + MIN_POWER)
+                .year(RANDOM.nextInt(MAX_YEAR_RANGE) + MIN_YEAR)
                 .build();
     }
 }
