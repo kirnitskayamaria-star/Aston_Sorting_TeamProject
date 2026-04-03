f02db96ae3263771ba8079776601f014da71ead9
AllanMercy <frostfire_1@mail.ru>
2026-04-03 11:42:06 +0300

    Подправил импорты в EvenSortStrategyTest

--- src/test/java/com/team/project/strategy/EvenSortStrategyTest.java
+++ src/test/java/com/team/project/strategy/EvenSortStrategyTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EvenSortStrategyTest {
 
