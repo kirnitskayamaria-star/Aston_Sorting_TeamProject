2d0cf6793cd254957b7700070356c7ed2007b084
Nikita Malinkin <nik.malina20@gmail.com>
2026-04-06 20:27:56 +0300

    В SearchService изменил сигнатуру метода. Теперь машины ищутся по модели

--- src/main/java/com/team/project/service/SearchService.java
+++ src/main/java/com/team/project/service/SearchService.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface SearchService {
 
-    Integer countCars(List<Car> cars, Car targetCar);
+    Integer countCars(List<Car> cars, String targetModel);
 }
--- src/main/java/com/team/project/service/impl/SearchServiceImpl.java
+++ src/main/java/com/team/project/service/impl/SearchServiceImpl.java
@@ -8,13 +8,13 @@ import java.util.List;
 public class SearchServiceImpl implements SearchService {
 
     @Override
-    public Integer countCars(List<Car> cars, Car targetCar) {
-        if (cars == null || cars.isEmpty() || targetCar == null) {
+    public Integer countCars(List<Car> cars, String targetModel) {
+        if (cars == null || cars.isEmpty() || targetModel == null) {
             return 0;
         }
 
         return (int) cars.parallelStream()
-                .filter(car -> car.equals(targetCar))
+                .filter(car -> car.getModel().equalsIgnoreCase(targetModel))
                 .count();
     }
 
